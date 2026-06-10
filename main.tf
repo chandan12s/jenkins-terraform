8c53540b32fedb492e00d1ab20d916c3c7bacfac
Chandan S <shivchandan1203@gmail.com>
2026-06-10 18:55:11 +0530

    changed ami

--- main.tf
+++ main.tf
@@ -20,13 +20,9 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
 }
 
 resource "aws_instance" "free_tier_instance" {
