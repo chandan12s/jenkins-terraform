6ec76f6a6c1ab8765525ca3796fa9c09bf812e4d
Chandan S <shivchandan1203@gmail.com>
2026-06-10 19:02:03 +0530

    changed ami to t3

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "free_tier_instance" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name        = "Jenkins-Terraform"
